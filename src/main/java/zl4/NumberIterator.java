818277e485b687f77c4d28b08f04ef3edb742015
Usuriii <the.svn1@gmail.com>
2026-09-04 16:35:22 +0200

    ZL-4: Stworzenie iteratora po liczbach

---
+++ src/main/java/zl4/NumberIterator.java
@@ -0,0 +1,17 @@
+package zl4;
+
+public class NumberIterator {
+    public static void main(String[] args) {
+        for (int i = 0; i < 30; i++) {
+           if (i % 15 == 0){
+               System.out.println("FizzBuzz");
+           } else if (i % 5 == 0) {
+               System.out.println("Buzz");
+           } else if (i % 3 == 0) {
+               System.out.println("Fizz");
+           } else {
+               System.out.println(i);
+           }
+        }
+    }
+}
